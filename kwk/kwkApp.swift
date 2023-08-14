b77cc277354240b73aca7ed3dda4615a14edaae0
Kayleigh McArdle <kaysmcardle@optonline.net>
2023-08-14 10:21:48 -0400

    Initial Commit

---
+++ kwk/kwkApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkApp.swift
+//  kwk
+//
+//  Created by scholar on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct kwkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
